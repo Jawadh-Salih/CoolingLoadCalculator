a5c7b9959dec6f1d473c9c07839ed515f38b3b97
PravindaPerera <pravinda.joes@gmail.com>
2016-08-16 18:48:44 +0530

    Sensible Load Calculation - window complete

--- functions/calculation.php
+++ functions/calculation.php
@@ -2,9 +2,9 @@
 
 require_once '../config/DB.php';
 
-//$wall varibles
-
 $sensible_load = array(0,0,0,0,0,0,0,0,0,0);
+
+//$wall varibles
 
 $wall_area = (double)0; //Will be the 'a' parameter
 $wall_u_value = (double)0;
@@ -19,8 +19,21 @@ $wall_int_tem = (double)0;
 $wall_ext_tem = (double)0;
 $wall_k_val = (double)0;
 
-//$window and $door
+//$window variables
 $window_area = (double)0;
+$wall_u_value = (double)0;
+
+$window_id;
+$win_wall_id;
+$window_width = (double)0;
+$window_height = (double)0;
+$window_thickness = (double)0;
+$win_wall_it;
+$window_int_tem = (double)0;
+$window_ext_tem = (double)0;
+$window_k_val = (double)0;
+
+//$window and $door
 $door_area = (double)0;
 
 //$constants
@@ -104,8 +117,8 @@ while($i<sizeof($result)){
 
     else{
         
-        for($x = 0; $x < 10; $x++) {
-           $sensible_load[$x] += ($wall_area * $wall_u_value * ($wall_ext_tem-$wall_int_tem));//Sally metana int-ext nathanm anik pathada?? Abs ganna weida??
+        for($x = 0; $x < $num_cltb; $x++) {
+           $sensible_load[$x] += ($wall_area * $wall_u_value * abs(($wall_ext_tem-$wall_int_tem)));
         }
     }
 
@@ -114,6 +127,55 @@ while($i<sizeof($result)){
 
 //Sensible load calculation - wall - done
 
+//Sensible load calculation - window - start
+
+$sql = "SELECT * FROM tbl_wall";
+$result = $DB->prepare($sql);
+
+$result->execute();
+$result = $result->fetchAll(PDO::FETCH_ASSOC);
+$i=0;
+while($i<sizeof($result)){
+//Foreign key eka tiyana nisa $win_wall_id mehema ganna puluwanda kiyala chk karapan machan 
+    $window_id = $result[$i]['win_id'];
+    $win_wall_id = $result[$i]['wall_id'];
+    $window_height = $result[$i]['height'];
+    $window_width = $result[$i]['width'];
+    $window_int_tem = $result[$i]['int_temp'];
+    $window_ext_tem = $result[$i]['ext_temp'];
+    $window_thickness = $result[$i]['thickness'];
+    $window_k_val = $result[$i]['k_val'];
+    print($window_id);
+    
+    $window_area = $window_height * $window_width;
+    $wall_u_value = (1/$h0) + ($window_thickness/$window_k_val) + (1/$h1);
+    
+    //Temperature difference calculation for sensible load window
+    for($x = 0; $x < $num_cltb; $x++) {
+           $sensible_load[$x] += ($wall_u_value * $window_area * abs(($window_ext_tem-$window_int_tem)));
+        }
+        
+    //CLTB calc for sensible load window calculation
+        //Need to get the direction of the window - for that look in which wall is this and obtained its direction
+        $sql_win_dir= "SELECT direction FROM wall WHERE wall_id = :wall_id";
+        $stmt = $DB->prepare($sql_win_dir);
+        $stmt->bindParam(':wall_id', $win_wall_id);
+        $stmt->execute();
+        
+        // provide the wall direction from the result of this stmt->execute() to the below querry 
+        
+        $sql_cltb_wall= "SELECT * FROM cltb_wall WHERE direction = :wall_direction";
+        $stmt = $DB->prepare($sql_cltb_wall);
+        $stmt->bindParam(':wall_direction', $wall_direction);
+        $stmt->execute();
+        
+        //Using the result of this loop through the CLTB values and add the results to the $sensible_load array
+        
+
+        //Do this again and again for all the windows of the table
+    $i++;
+}
+//Sensible load calculation - window - done
 
 
 $url = "../results.html";
